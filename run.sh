fb7ccde8df415bf58be26d70e3401dd64137816f
Jozef Mlich <xmlich02@stud.fit.vutbr.cz>
2013-08-14 12:59:03 +0200

    screensaver and powersaving

--- run.sh
+++ run.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
+
+## disable screensaver
+
+#killall gnome-screensaver
+#killall mate-screensaver
+#killall xscreensaver
+
+## disable power saving
+
+#xset -dpms
+#xset dpms off
+#xset s noblank
+#xset s off
+#xset dpms force off
+
+
 #~/Qt5.0.0/5.0.0/gcc/bin/qmlscene --fullscreen ./twitterWall.qml 
 ~/Qt5.0.1/5.0.1/gcc/bin/qmlscene --fullscreen ./twitterWall.qml 
 #~/Qt5.0.1/5.0.1/gcc/bin/qmlscene ./twitterWall.qml 
